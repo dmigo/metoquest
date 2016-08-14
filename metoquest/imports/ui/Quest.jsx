e807592caa69b8b285052a5d540ce77113872c38
Dmitry Goryunov <d.f.goryunov@gmail.com>
2016-08-14 16:35:49 +0200

    Add update and remove

--- metoquest/imports/ui/Quest.jsx
+++ metoquest/imports/ui/Quest.jsx
@@ -1,8 +1,32 @@
 import React, {Component, PropTypes} from 'react'
+import {Quests} from '../api/quests'
 
 export default class Quest extends Component {
+    toggleDone() {
+        Quests.update(this.props.quest._id, {
+            $set: {done: !this.props.quest.done}
+        })
+    }
+
+    delete() {
+        Quests.remove(this.props.quest._id)
+    }
+
     render() {
-        return (<li>{this.props.quest.text}</li>)
+        const className = this.props.quest.done ? 'done' : ''
+        return (<li className={className}>
+                <button className="delete" onClick={this.delete.bind(this)}>
+                    &times;
+                </button>
+                <input
+                    type="checkbox"
+                    readOnly
+                    checked={this.props.quest.done}
+                    onClick={this.toggleDone.bind(this)}
+                />
+                <span className="text">{this.props.quest.text}</span>
+            </li>
+        )
     }
 }
 
